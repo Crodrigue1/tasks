3ade5daeab857815cacb1f25d40920c8557623ac
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-10-11 16:48:26 -0400

    Task 2: Added list with three elements

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,13 @@ function App(): JSX.Element {
             <header className="App-header">
                 COS420 with React Hooks and TypeScript, Collin Rodrigue
             </header>
+            <p>
+                <ul>
+                    <li>Dogs</li>
+                    <li>Cats</li>
+                    <li>Fish</li>
+                </ul>
+            </p>
         </div>
     );
 }
