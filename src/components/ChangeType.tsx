10ebddfceaef621b721ce1dcfaf860bf86d2fbc0
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-10-25 19:00:51 -0400

    Completed ChangeType

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [starttype, changetype] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function newtype(): void {
+        if (starttype === "short_answer_question") {
+            changetype("multiple_choice_question");
+        } else {
+            changetype("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={newtype}>Change Type</Button>
+            {starttype === "short_answer_question" ? (
+                <div>Short Answer</div>
+            ) : (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
 }
