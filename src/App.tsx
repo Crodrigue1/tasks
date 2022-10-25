1a8f3c28dddab1fed50613b5ab5aeea615d06db4
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-10-25 11:00:55 -0400

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,53 +11,22 @@ import { Counter } from "./components/Counter";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <h1>heading</h1>
             <header className="App-header">
                 COS420,There is a header, Collin Rodrigue,
             </header>
-            <div>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-            </div>
-            <p>
-                <ul>
-                    <li>Dogs</li>
-                    <li>Cats</li>
-                    <li>Fish</li>
-                </ul>
-            </p>
-            <div className="App">
-                <Container>
-                    <Row>
-                        <Col>
-                            <h2 className="App-rectangle">Column 1,</h2>
-                        </Col>
-
-                        <Col>
-                            <h3 className="App-rectangle">Column 2</h3>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-            <div>
-                <img src="public\coast.jpg" alt="Coast" />
-            </div>
-            </div>
-                </header>
-                <hr></hr>
-                <Counter></Counter>
-                <hr />
-                <RevealAnswer></RevealAnswer>
-                <hr />
-                <StartAttempt></StartAttempt>
-                <hr />
-                <TwoDice></TwoDice>
-                <hr />
-                <ChangeType></ChangeType>
-                <hr />
-                <CycleHoliday></CycleHoliday>
-            </div>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
+        </div>
     );
 }
 
