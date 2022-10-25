3431210be8caec9771ab961beaff83536aa51671
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-10-25 17:16:06 -0400

    Completed Reveal Answers

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,15 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [vis, newvis] = useState<boolean>(false);
+
+    function changevis(): void {
+        newvis(!vis);
+    }
+    return (
+        <div>
+            <Button onClick={changevis}>Reveal Answer</Button>
+            {vis && <div>42</div>}
+        </div>
+    );
 }
