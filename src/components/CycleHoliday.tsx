ded5c7658ded04fcdd3d9325c60b41f3063dcff4
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-10-25 21:44:51 -0400

    Completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,41 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-
+//Emoji's 🐑, 🎄,❄️️,😇, 🎅
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, newHoliday] = useState<string>("🐑");
+    function alphabetical(): void {
+        if (holiday === "🐑") {
+            newHoliday("🎅");
+        } else if (holiday === "🎅") {
+            newHoliday("😇");
+        } else if (holiday === "😇") {
+            newHoliday("❄️");
+        } else if (holiday === "❄️") {
+            newHoliday("🎄");
+        } else {
+            newHoliday("🐑");
+        }
+    }
+    function time(): void {
+        if (holiday === "❄️") {
+            newHoliday("🐑");
+        } else if (holiday === "🐑") {
+            newHoliday("🎄");
+        } else if (holiday === "🎄") {
+            newHoliday("🎅");
+        } else if (holiday === "🎅") {
+            newHoliday("😇");
+        } else {
+            newHoliday("❄️");
+        }
+    }
+    return (
+        <div>
+            <div>
+                <span>Holiday: {holiday}</span>
+            </div>
+            <Button onClick={alphabetical}>Advance by Alphabet</Button>
+            <Button onClick={time}>Advance by Year</Button>
+        </div>
+    );
 }
