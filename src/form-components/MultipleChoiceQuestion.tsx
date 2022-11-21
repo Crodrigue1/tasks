671442a168dd0687905a1fba4d01a8b49f1b504d
Collin Rodrigue <collin.rodrigue@maine.edu>
2022-11-20 21:43:57 -0500

    Completed MultipleChoiceQuestion

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [question, updateQuestion] = useState<string>(options[0]);
+    function changeQuestion(event: React.ChangeEvent<HTMLSelectElement>) {
+        updateQuestion(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formQuestion">
+                <Form.Label>Choose Question Answer Below</Form.Label>
+                <Form.Select value={question} onChange={changeQuestion}>
+                    {options.map((question: string) => (
+                        <option key={question} value={question}>
+                            {question}
+                        </option>
+                    ))}
+                </Form.Select>
+                <option>{question === expectedAnswer ? "✔️" : "❌"}</option>
+            </Form.Group>
         </div>
     );
 }
